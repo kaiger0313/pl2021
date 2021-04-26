54a0f422b2f84a16202e77614a6a8d7e522bdfa5
chomskim <csknova@gmail.com>
2021-04-26 13:18:42 +0900

    add groovy program

--- 2-oop/shape/shape.js
+++ 2-oop/shape/shape.js
@@ -3,47 +3,47 @@ class Shape {
 }
 class Circle extends Shape {
   constructor(r) {
-    super();
-    this.radius = r;
+    super()
+    this.radius = r
   }
   toString() {
-    return `Circle Radius=${this.radius}`;
+    return `Circle Radius=${this.radius}`
   }
   computeArea() {
-    return Math.PI * this.radius * this.radius;
+    return Math.PI * this.radius * this.radius
   }
 }
 class Rectangle extends Shape {
   constructor(w, h) {
-    super();
-    this.height = h;
-    this.width = w;
+    super()
+    this.height = h
+    this.width = w
   }
   toString() {
-    return `Rectangle Width=${this.width}, Height=${this.height}`;
+    return `Rectangle Width=${this.width}, Height=${this.height}`
   }
   computeArea() {
-    return this.width * this.height;
+    return this.width * this.height
   }
 }
-function totalAreas(allShapes) {
-  let total = 0.0;
-  for (let i = 0; i < allShapes.length; ++i) {
-    const s = allShapes[i];
-    console.log(`${s.toString()} -- area=${s.computeArea()}`);
-    total += s.computeArea();
+function totalAreas(shapes) {
+  let total = 0.0
+  for (let i = 0; i < shapes.length; ++i) {
+    const s = shapes[i]
+    console.log(`${s.toString()} -- area=${s.computeArea()}`)
+    total += s.computeArea()
   }
-  return total;
+  return total
 }
-let allShapes = [];
-allShapes.push(new Rectangle(2, 3));
-allShapes.push(new Circle(5));
-allShapes.push(new Rectangle(5.0, 5.0));
-allShapes.push(new Circle(8.0));
-allShapes.push(new Rectangle(9, 9));
-allShapes.push(new Circle(0));
-allShapes.push(new Rectangle(0, 0));
+let allShapes = []
+allShapes.push(new Rectangle(2, 3))
+allShapes.push(new Circle(5))
+allShapes.push(new Rectangle(5.0, 5.0))
+allShapes.push(new Circle(8.0))
+allShapes.push(new Rectangle(9, 9))
+allShapes.push(new Circle(0))
+allShapes.push(new Rectangle(0, 0))
 
-console.log(`allShapes = ${JSON.stringify(allShapes)}`);
-const tot = totalAreas(allShapes);
-console.log(`Total Area = ${tot}`);
+console.log(`allShapes = ${JSON.stringify(allShapes)}`)
+const tot = totalAreas(allShapes)
+console.log(`Total Area = ${tot}`)
