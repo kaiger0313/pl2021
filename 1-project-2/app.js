182f6135e3a2b67c08731fe2895cbac9b5aa81b6
chomskim <csknova@gmail.com>
2021-04-28 12:36:57 +0900

    add report 2

--- 1-project-2/app.js
+++ 1-project-2/app.js
@@ -1,16 +1,37 @@
-let contactList = RANDOM_USERS.results.map((user) => {
-  const name = `${user.name.first} ${user.name.last}`
-  const email = user.email
-  const phone = user.phone
-  return { name, email, phone }
-})
+// Project 2 -- Use Local Storage(localStore)
 
-console.log(JSON.stringify(contactList))
+let CONTACT_DB = null
 
-let CONTACT_DB = []
+function saveDB() {
+  console.log(JSON.stringify(CONTACT_DB))
+  localStorage.setItem('CONTACT_DB', JSON.stringify(CONTACT_DB))
+}
+
+function restoreDB() {
+  let db = localStorage.getItem('CONTACT_DB')
+  CONTACT_DB = db ? JSON.parse(db) : null
+}
+
+function init() {
+  restoreDB()
+  console.log('CONTACT_DB=', JSON.stringify(CONTACT_DB))
+  if (CONTACT_DB === null) {
+    let contactList = RANDOM_USERS.results.map((user) => {
+      const name = `${user.name.first} ${user.name.last}`
+      const email = user.email
+      const phone = user.phone
+      return { name, email, phone }
+    })
+    CONTACT_DB = []
+    contactList.forEach((row) => {
+      create(row)
+    })
+  }
+}
 
 function create(row) {
   CONTACT_DB.push(row)
+  saveDB()
 }
 
 function read(keyObj) {
@@ -33,33 +54,42 @@ function update(keyObj, valueObj) {
   updMember = { ...updMember, ...valueObj }
   console.log(updMember)
   CONTACT_DB = [...newDB, updMember]
+  saveDB()
 }
 
 function remove(keyObj) {
   const newDB = CONTACT_DB.filter((row) => row.name !== keyObj.name)
   CONTACT_DB = [...newDB]
+  saveDB()
 }
 
 function list() {
-  const tbody = document.querySelector('#contact-table-body')
-  const tbodyText = CONTACT_DB.map((tr) => {
-    return `<tr><td>${tr.name}</td><td>${tr.email}</td><td>${tr.phone}</td></tr>`
-  }).join('\n')
-  console.log(tbodyText)
-  tbody.innerHTML = tbodyText
+  return CONTACT_DB
 }
+
 function listConsole() {
-  CONTACT_DB.forEach((row) => {
+  list().forEach((row) => {
     console.log(JSON.stringify(row))
   })
 }
+
+function listHTML() {
+  const tbody = document.querySelector('#contact-table-body')
+  const tbodyText = list()
+    .map((tr) => {
+      return `<tr><td>${tr.name}</td><td>${tr.email}</td><td>${tr.phone}</td></tr>`
+    })
+    .join('\n')
+  console.log(tbodyText)
+  tbody.innerHTML = tbodyText
+}
+
 // Main Routine
 
-// build CONTACT_DB
-contactList.forEach((row) => {
-  create(row)
-})
+init()
+
 console.log('After Initial Create')
+
 listConsole()
 create({ name: 'cskim', email: 'cskim@hufs.ac.kr', phone: '031-330-4365' })
 create({ name: 'cskim', email: 'cskim@hufs-gsuite.kr', phone: '010-111-1234' })
@@ -72,8 +102,8 @@ update({ name: 'cskim' }, { phone: '010-555-5555' })
 console.log('After Update cskim')
 listConsole()
 
-remove({ name: 'cskim' })
-console.log('After Remove cskim')
-listConsole()
+//remove({ name: 'cskim' })
+//console.log('After Remove cskim')
+//listConsole()
 
-list()
+listHTML()
